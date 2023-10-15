dcd7aeaeb665c90c9c3a907c6f1cbbf87a91eba0
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-15 17:54:13 +0000

    dice homepage setup

--- dice.rb
+++ dice.rb
@@ -2,9 +2,23 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello world"
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -16,17 +30,17 @@ get("/giraffe") do
 end
 
 
-get("/diceroll") do
+#get("/diceroll") do
  # <html>
  # <head>
-  "<h1> Dice rolls </h1>"
-  "<p> <a href="/dice/2/6">2d6</a> </p>"
+ # "<h1> Dice rolls </h1>"
+  #<p> <a href="/dice/2/6">2d6</a> </p>
  # <ul>
   #  <li><a href="/dice/2/6">2d6</a></li>
   #</ul>
   #</body>
   #</html>
-end
+#end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
