b66fb6d43295b06ffa42a758505b4d2ec5ad9904
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-15 18:04:09 +0000

    elephant.erb created

--- dice.rb
+++ dice.rb
@@ -10,15 +10,16 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
+  #"
+  #<h1>Dice Roll</h1>
+  #<ul>
+   # <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    #<li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+   # <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+   # <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  #</ul>
+  #"
 end
 
 get("/zebra") do
@@ -29,18 +30,6 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server :fingerscrossed"
 end
 
-
-#get("/diceroll") do
- # <html>
- # <head>
- # "<h1> Dice rolls </h1>"
-  #<p> <a href="/dice/2/6">2d6</a> </p>
- # <ul>
-  #  <li><a href="/dice/2/6">2d6</a></li>
-  #</ul>
-  #</body>
-  #</html>
-#end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
